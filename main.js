684f238c853cd8d78f1731d32641860d4c8e83b2
AliMo06 <108025201+AliMo06@users.noreply.github.com>
2024-10-12 18:10:18 -0400

    basically made it functional somewhat
    yes

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const axios = require('axios');
 
 const riot_api = process.env.RIOT_API_KEY;
 const gameName = "Kabir Wahid"
 const tagLine = "BIG"
 
+
+let puuid;  //puuid
+let sumid;  //summoner ID
+let sdTier  //solo duo tier
+let sdRank; //solo duo rank
+
 const riot_url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${riot_api}`;
 
 axios.get(riot_url)
     .then(response => {
         const dataJson = response.data;
-        const puuid = dataJson['puuid'];
-        console.log(puuid);
+        puuid = dataJson['puuid'];
+
+
+        const sum_id_url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${encodeURIComponent(puuid)}?api_key=${riot_api}`;
+
+        return axios.get(sum_id_url);
+    })
+    .then(response => {
+        const dataJson = response.data;
+        sumid = dataJson['id'];
+        rank_url = `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(sumid)}?api_key=${riot_api}`;
+        return axios.get(rank_url);
+    })
+    .then(response => {
+        const dataJson = response.data[0];
+        sdTier = dataJson['tier'];
+        sdRank = dataJson['rank'];
+        console.log(sdTier)
+        console.log(sdRank)
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
+
+
