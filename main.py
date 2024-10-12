2f80eff257cbd7501a2577f43f6c787d919e1d1f
jjajaw <166040155+jjajaw@users.noreply.github.com>
2024-10-12 15:32:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,18 @@ load_dotenv()
 
 riot_api = os.getenv('RIOT_API_KEY')
 
-#/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine} for input of riotID/tagline 
+api_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/bdchris18/na1?api_key=RGAPI-d72b6bcb-da6e-47ad-96d0-a36bb776d372"
+
+import requests
+
+requests.get(api_url)
+
+api_url =api_url + '?api_key=' + riot_api
+
+resp = requests.get(api_url)
+player_info = resp.json()
+
+print(resp, player_info)
+
+
+
