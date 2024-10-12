ef931c22795e68baf2d0f0464892478c75215892
jjajaw <166040155+jjajaw@users.noreply.github.com>
2024-10-12 15:58:03 -0400

    puuid variable added
    blah

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 riot_api = os.getenv('RIOT_API_KEY')
 
-api_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/bdchris18/na1?api_key=RGAPI-d72b6bcb-da6e-47ad-96d0-a36bb776d372"
+api_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/bdchris18/na1"
 
 import requests
 
@@ -14,8 +14,12 @@ api_url =api_url + '?api_key=' + riot_api
 
 resp = requests.get(api_url)
 player_info = resp.json()
-
 print(resp, player_info)
+
+dataObject = requests.get(api_url)
+dataJson = dataObject.json()
+puuid = dataJson['puuid']
+print(puuid)
 
 
 
