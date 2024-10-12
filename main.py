24520abde2750b02d4085699152bf8c099261209
AliMo06 <108025201+AliMo06@users.noreply.github.com>
2024-10-12 15:30:53 -0400

    URL for accounts fixed
    URL

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 import os
+import requests 
+
 load_dotenv()
 
 riot_api = os.getenv('RIOT_API_KEY')
+riot_url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/Kabir%20Wahid/BIG?api_key=" + riot_api
 
 #/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine} for input of riotID/tagline 
+x = requests.get(riot_url)
+print(x.json()) #gives puuid, gamename, and tag
