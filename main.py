967c6d761237b86644d50c105f3806b1bcfaab0a
AliMo06 <108025201+AliMo06@users.noreply.github.com>
2024-10-12 15:56:22 -0400

    puuid given as variable
    yippe

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ riot_api = os.getenv('RIOT_API_KEY')
 riot_url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/Kabir%20Wahid/BIG?api_key=" + riot_api
 
 #/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine} for input of riotID/tagline 
-x = requests.get(riot_url)
-print(x.json()) #gives puuid, gamename, and tag
+dataObject = requests.get(riot_url)
+dataJson = dataObject.json()
+puuid = dataJson['puuid']
+
+
