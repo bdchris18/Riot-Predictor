e8562d41478b8902a6289ee06f8e81fcbb7f6b15
AliMo06 <108025201+AliMo06@users.noreply.github.com>
2024-10-17 15:07:55 -0400

    Good code now, it finally does somethign

--- main.js
+++ main.js
@@ -2,70 +2,86 @@ require('dotenv').config();
 const axios = require('axios');
 
 const riot_api = process.env.RIOT_API_KEY;
-const gameName = "Sirflash4"
-const tagLine = "na1"
+const gameName = "Leviatã";
+const tagLine = "777";
 
+let puuid, sumid, sdTier, sdRank, sdWins, sdLosses, sdLP;
+let matchList, singleMatch, playerList, matchName, matchTag;
 
-let puuid, sumid, sdTier, sdRank, sdWins, sdLosses, sdLP; // Player Identification and Ranked Stats
-let singleMatch, playerList, matchName, matchTag; //Match Variables
 const riot_url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${riot_api}`;
 
-axios.get(riot_url)
-    .then(response => {
+// Wait function
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Slow down async function
+async function slowDown(num) {
+    await wait(num * 1000);  // Wait for num seconds
+}
+
+// API Executer
+async function fetchData() {
+    try {
+        // Get PUUID
+        const response = await axios.get(riot_url);
         const dataJson = response.data;
         puuid = dataJson['puuid'];
 
-
+        // Get Summoner ID
         const sum_id_url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${encodeURIComponent(puuid)}?api_key=${riot_api}`;
+        const summonerResponse = await axios.get(sum_id_url);
+        sumid = summonerResponse.data['id'];
 
-        return axios.get(sum_id_url);
-    })
-    .then(response => {
-        const dataJson = response.data;
-        sumid = dataJson['id'];
-        rank_url = `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(sumid)}?api_key=${riot_api}`;
-        return axios.get(rank_url);
-    })
-    .then(response => {
-        const dataJson = response.data[0];
-        sdTier = dataJson['tier'];
-        sdRank = dataJson['rank'];
-        sdLP = dataJson['leaguePoints']
+        // Get Rank Info
+        const rank_url = `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(sumid)}?api_key=${riot_api}`;
+        const rankResponse = await axios.get(rank_url);
+        const rankData = rankResponse.data[0];
+        sdTier = rankData['tier'];
+        sdRank = rankData['rank'];
+        sdLP = rankData['leaguePoints'];
+        sdWins = rankData['wins'];
+        sdLosses = rankData['losses'];
 
-        sdWins = dataJson['wins'];
-        sdLosses = dataJson['losses'];
+        console.log(sdTier + " " + sdRank + " " + sdLP + "LP");
+        console.log(sdWins + "W " + sdLosses + "L");
 
-        console.log(sdTier + " " + sdRank + " " + sdLP + "LP")
-        console.log(sdWins + "W" + " " + sdLosses + "L")
-
+        // Get Match List
         const match_url = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${encodeURIComponent(puuid)}/ids?start=0&count=20&api_key=${riot_api}`;
-        return axios.get(match_url);
-    })
-    .then(response => {
-        const dataJson = response.data;
-        singleMatch = response.data[0]; //this will be a matchList
-        singleMatch = dataJson[0];
-        const single_Match_URL = `https://americas.api.riotgames.com/lol/match/v5/matches/${encodeURIComponent(singleMatch)}?api_key=${riot_api}`
-        return axios.get(single_Match_URL); //gets data about a single match
-    })
-    .then(response => {
-        const dataJson = response.data;
-        playerList = dataJson['metadata']['participants'];
+        const matchResponse = await axios.get(match_url);
+        matchList = matchResponse.data;
 
-        for (let i = 0; i < playerList.length; i++) {
-            iterativePuuid = playerList[i];
-            const account_Retrival_URL = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/${encodeURIComponent(iterativePuuid)}?api_key=${riot_api}`;
-            axios.get(account_Retrival_URL)
-                .then(response => {
-                    const dataJson = response.data;
-                    matchName = dataJson['gameName'];
-                    matchTag = dataJson['tagLine'];
-                    console.log(matchName + "#" + matchTag)
-                })
+        for (let j = 0; j < 4; j++) {
+            singleMatch = matchList[j];
+            const single_Match_URL = `https://americas.api.riotgames.com/lol/match/v5/matches/${encodeURIComponent(singleMatch)}?api_key=${riot_api}`;
+            const singleMatchResponse = await axios.get(single_Match_URL);
+            playerList = singleMatchResponse.data['metadata']['participants'];
+
+            console.log("\n\nGame " + (j + 1));
+
+            for (let i = 0; i < playerList.length; i++) {
+                const iterativePuuid = playerList[i];
+                const account_Retrival_URL = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/${encodeURIComponent(iterativePuuid)}?api_key=${riot_api}`;
+                const accountResponse = await axios.get(account_Retrival_URL);
+                const accountData = accountResponse.data;
+                matchName = accountData['gameName'];
+                matchTag = accountData['tagLine'];
+                if (i == 0) {
+                    console.log("\nBlue Team:")
+                }
+                if (i == 5) {
+                    console.log("\nRed Team:")
+                }
+                console.log(matchName + "#" + matchTag);
+
+            }
+
+            await slowDown(2); // Slow down loop
         }
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Error fetching data:', error);
-    });
+    }
+}
 
 
+fetchData();
