a7e831a8e6475459a15ac9b96133ed9376da0ece
AliMo06 <108025201+AliMo06@users.noreply.github.com>
2024-10-17 14:34:15 -0400

    Changed it so that you can print all players in a given game, can iterate and print all from the last 20, but not there yet

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const axios = require('axios');
 
 const riot_api = process.env.RIOT_API_KEY;
-const gameName = "Kabir Wahid"
-const tagLine = "BIG"
+const gameName = "Sirflash4"
+const tagLine = "na1"
 
 
 let puuid, sumid, sdTier, sdRank, sdWins, sdLosses, sdLP; // Player Identification and Ranked Stats
-let matchList, singleMatch; //Match Variables
+let singleMatch, playerList, matchName, matchTag; //Match Variables
 const riot_url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${riot_api}`;
 
 axios.get(riot_url)
@@ -42,9 +42,27 @@ axios.get(riot_url)
         return axios.get(match_url);
     })
     .then(response => {
-        const dataJson = response.data; //this will be a matchList
-        const single_Match_URL = `https://americas.api.riotgames.com/lol/match/v5/matches/${encodeURIComponent(singleMatch)}1?api_key=${riot_api}`
+        const dataJson = response.data;
+        singleMatch = response.data[0]; //this will be a matchList
+        singleMatch = dataJson[0];
+        const single_Match_URL = `https://americas.api.riotgames.com/lol/match/v5/matches/${encodeURIComponent(singleMatch)}?api_key=${riot_api}`
+        return axios.get(single_Match_URL); //gets data about a single match
+    })
+    .then(response => {
+        const dataJson = response.data;
+        playerList = dataJson['metadata']['participants'];
 
+        for (let i = 0; i < playerList.length; i++) {
+            iterativePuuid = playerList[i];
+            const account_Retrival_URL = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/${encodeURIComponent(iterativePuuid)}?api_key=${riot_api}`;
+            axios.get(account_Retrival_URL)
+                .then(response => {
+                    const dataJson = response.data;
+                    matchName = dataJson['gameName'];
+                    matchTag = dataJson['tagLine'];
+                    console.log(matchName + "#" + matchTag)
+                })
+        }
     })
     .catch(error => {
         console.error('Error fetching data:', error);
